7628f24cd17cb3a96d098b5e70a7856bad76a0d7
Kardkasem Saenyen <meoss@Kardkasems-MacBook-Air.local>
2019-04-24 12:39:18 +1000

    add emoji

--- zodiac.rb
+++ zodiac.rb
@@ -1,5 +1,5 @@
 #Zodiac.rb
-%w{colorize zodiac httparty tty-prompt ./api.rb ./module.rb}.each { |x| require x }
+%w{colorize zodiac httparty tty-prompt ./api.rb ./module.rb ruby_emoji}.each { |x| require x }
 
 prompt = TTY::Prompt.new    #initiate tty-prompt class
 
@@ -9,10 +9,11 @@ zodiac = Zodiac::get_wzodiac(date, month, year)
 puts(Utility::random_color("\nYour Zodiac is ") + Utility::random_color(zodiac).underline.bold)
 
 choice = ""
-
 while (choice != "quit")
   choice = prompt.select("\nWhat whould you like to know about your " +
-                         "#{zodiac}".green + "\nPlease select?".blue,
+                         RubyEmoji.parse("#{emoji(zodiac.downcase.to_sym)}").yellow + " " +
+                         "#{zodiac}".green + " " + RubyEmoji.parse("#{emoji(zodiac.downcase.to_sym)}").yellow +
+                         +"\nPlease select?".blue,
                          %w( Learn MatchZodiac Horoscope ChineseZodiac quit ))
   case choice
   when "Learn"
@@ -24,6 +25,7 @@ while (choice != "quit")
   when "ChineseZodiac"
     puts("Chinese Zodiac : " + Zodiac::get_czodiac(year).yellow)
   when "quit"
+    system "clear"
     break
   end
 end
